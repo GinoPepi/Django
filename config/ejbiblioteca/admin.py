fc81027cb04f0a86d2c733c80261369915f60be2
GinoPepi <pepi.gino@gmail.com>
2020-05-14 20:57:34 -0300

    cracion de clases y registro en el admin

--- config/ejbiblioteca/admin.py
+++ config/ejbiblioteca/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from ejbiblioteca.models import Autor
+from ejbiblioteca.models import Libro
+from ejbiblioteca.models import Ejemplar
+from ejbiblioteca.models import Usuario
+
 
 # Register your models here.
+admin.site.register (Autor,)
+admin.site.register (Libro,)
+admin.site.register (Ejemplar,)
+admin.site.register (Usuario,)
